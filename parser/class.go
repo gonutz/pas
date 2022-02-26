7f9cc3a5c9b7f6245360f4ecaa884ab757950fe1
akm <akm2000@gmail.com>
2022-02-26 20:54:12 +0900

    :sparkles: Consider class function, class procedure and class field

--- parser/class.go
+++ parser/class.go
@@ -9,34 +9,54 @@ func classProcessor(class *ast.Class) func(p *parser) error {
 			return nil
 		}
 	}
-	appendMethod := func(p *parser) error {
-		f, err := p.parseFunctionDeclaration()
-		if err != nil {
-			return err
+	appendMethod := func(static bool) func(p *parser) error {
+		return func(p *parser) error {
+			f, err := p.parseFunctionDeclaration()
+			if err != nil {
+				return err
+			}
+			class.AppendMemberToCurrentSection(&ast.Method{
+				Class:    static,
+				Function: *f,
+			})
+			return nil
 		}
-		class.AppendMemberToCurrentSection(&ast.Method{
-			Function: *f,
-		})
-		return nil
 	}
-	appendField := func(p *parser) error {
-		v, err := p.parseVariableDeclaration()
-		if err != nil {
-			return err
+
+	appendField := func(static bool) func(p *parser) error {
+		return func(p *parser) error {
+			v, err := p.parseVariableDeclaration()
+			if err != nil {
+				return err
+			}
+			class.AppendMemberToCurrentSection(&ast.Field{
+				Class:    static,
+				Variable: *v,
+			})
+			return nil
 		}
-		class.AppendMemberToCurrentSection(&ast.Field{Variable: *v})
-		return nil
 	}
+
+	classSelector := &procSelector{
+		procs: []*namedProc{
+			{"procedure", appendMethod(true)},
+			{"function", appendMethod(true)},
+			{"var", appendField(true)},
+		},
+		defaultProc: appendField(true),
+	}
+
 	selector := &procSelector{
 		procs: []*namedProc{
 			{"published", newSection(ast.Published)},
 			{"public", newSection(ast.Public)},
 			{"protected", newSection(ast.Protected)},
 			{"private", newSection(ast.Private)},
-			{"procedure", appendMethod},
-			{"function", appendMethod},
+			{"procedure", appendMethod(false)},
+			{"function", appendMethod(false)},
+			{"class", classSelector.Do},
 		},
-		defaultProc: appendField,
+		defaultProc: appendField(false),
 	}
 	return func(p *parser) error {
 		if err := p.eatWord("class"); err != nil {
