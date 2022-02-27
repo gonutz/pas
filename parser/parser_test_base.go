c13fba736430b55d61a7ffbb59fe6b0cf0f3539b
akm <akm2000@gmail.com>
2022-02-27 09:25:01 +0900

    :green_heart: Show string difference on failure

--- parser/parser_test_base.go
+++ parser/parser_test_base.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"encoding/json"
 	"strings"
 	"testing"
 
@@ -8,12 +9,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func parseFile(t *testing.T, code string, want *ast.File) {
+func parseFile(t *testing.T, code string, expected *ast.File) {
 	t.Helper()
 	code = strings.Replace(code, "\n", "\r\n", -1)
-	f, err := ParseString(code)
+	actual, err := ParseString(code)
 	if err != nil {
 		t.Fatalf("%+v\n", err)
 	}
-	assert.Equal(t, want, f)
+	if !assert.Equal(t, expected, actual) {
+		expectedJson, err := json.MarshalIndent(expected, "", "  ")
+		assert.NoError(t, err)
+		actualJson, err := json.MarshalIndent(actual, "", "  ")
+		assert.NoError(t, err)
+		assert.Equal(t, string(expectedJson), string(actualJson))
+	}
 }
