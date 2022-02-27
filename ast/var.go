58fc5c68307363831072eb977796d47d6f6b41ec
akm <akm2000@gmail.com>
2022-02-27 21:29:38 +0900

    :green_heart: Add attributes to Variable
    And define ThreadVar also

--- ast/var.go
+++ ast/var.go
@@ -4,7 +4,28 @@ func (VarBlock) isFileSectionBlock() {}
 
 type VarBlock []*Variable
 
+func (ThreadVarBlock) isFileSectionBlock() {}
+
+type ThreadVarBlock []*Variable
+
 type Variable struct {
-	Name string
-	Type string
+	Names    []string
+	Type     string
+	Default  string
+	Length   int
+	Absolute string
+}
+
+func NewVariable(name string, nameOrType string, nameOrTypes ...string) *Variable {
+	var names []string
+	var typ string
+	if len(nameOrTypes) == 0 {
+		names = []string{name}
+		typ = nameOrType
+	} else {
+		l := len(nameOrTypes)
+		typ = nameOrTypes[l-1]
+		names = append([]string{name, nameOrType}, nameOrTypes[:l-1]...)
+	}
+	return &Variable{Names: names, Type: typ}
 }
